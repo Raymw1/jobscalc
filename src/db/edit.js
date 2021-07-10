b6e205c6bdb1401bb705d76932ded54ace71de86
Rayan Wilbert <rayan.rmw@gmail.com>
2021-07-09 22:29:49 -0300

    DB creation done, and edit profile in DB function done

--- src/db/edit.js
+++ src/db/edit.js
@@ -1,22 +1,44 @@
 const Database = require("./config");
 
-const a = {
-  async insertingProfile() {
-    const db = await Database();
-    await db.run(`INSERT INTO profile 
-        (name, avatar, monthly_budget, days_per_week, hours_per_day, vacation_per_year, value_hour)
-        VALUES ("Rayan Wilbert", "https://github.com/raymw1.png", 3000, 5, 5, 4, 75)
-        ;`);
-    await db.close();
+module.exports = {
+  inserting: {
+    async insertingProfile() {
+      const db = await Database();
+      await db.run(`INSERT INTO profile 
+          (name, avatar, monthly_budget, days_per_week, hours_per_day, vacation_per_year, value_hour)
+          VALUES ("Rayan Wilbert", "https://github.com/raymw1.png", 3000, 5, 5, 4, 75)
+          ;`);
+      await db.close();
+    },
+    async insertingJob(data) {
+      const db = await Database();
+      // await db.run(`INSERT INTO jobs 
+      //     (name, daily_hours, total_hours, created_at, profile_id)
+      //     VALUES (${}, ${}, ${}, ${}, ${})
+      //     ;`);
+      await db.close();
+    }
   },
-  async insertingJob() {
-    const db = await Database();
-    await db.run(`INSERT INTO jobs 
-        (name, daily_hours, total_hours, created_at, profile_id)
-        VALUES ("Pozzaria Guloso", 2, 1, 1617514376018, 2)
-        ;`);
-    await db.close();
+  updating: {
+    async updateProfile(data) {
+      const db = await Database();
+      await db.run(`UPDATE profile SET
+          name = "${data.name}", avatar = "${data.avatar}", monthly_budget = ${data["monthly-budget"]}, 
+          days_per_week = ${data["days-per-week"]},hours_per_day = ${data["hours-per-day"]}, 
+          vacation_per_year = ${data["vacation-per-year"]}, value_hour = ${data["value-hour"]}
+          ;`);
+      await db.close();
+    },
+    // async updateJob(data) {
+    //   const db = await Database();
+    //   await db.run(`UPDATE profile SET
+    //       name = "${data.name}", avatar = "${data.avatar}", monthly_budget = ${data["monthly-budget"]}, 
+    //       days_per_week = ${data["days-per-week"]},hours_per_day = ${data["hours-per-day"]}, 
+    //       vacation_per_year = ${data["vacation-per-year"]}, value_hour = ${data["value-hour"]}
+    //       ;`);
+    //   await db.close();
+    // }
   }
 };
 
-a.insertingProfile();
+// a.insertingProfile();
--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,18 +1,22 @@
-let data = {
-  name: "Rayan",
-  avatar: "https://github.com/raymw1.png",
-  "monthly-budget": 3000,
-  "days-per-week": 5,
-  "hours-per-day": 8,
-  "vacation-per-year": 4,
-  "value-hour": 18.75,
-};
+const Database = require("../db/config");
+const editDb = require("../db/edit");
 
 module.exports = {
-  get() {
-    return data;
+  async get() {
+    const db = await Database();
+    let data = await db.get(`SELECT * FROM profile;`);
+    await db.close();
+    return {
+      name: data.name,
+      avatar: data.avatar,
+      "monthly-budget": data.monthly_budget,
+      "days-per-week": data.days_per_week,
+      "hours-per-day": data.hours_per_day,
+      "vacation-per-year": data.vacation_per_year,
+      "value-hour": data.value_hour,
+    };
   },
-  update(newData) {
-    data = newData;
-  }
+  async update(newData) {
+    editDb.updating.updateProfile(newData);
+  },
 };
